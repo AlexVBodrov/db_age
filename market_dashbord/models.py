fe08ac5ac52a818983abc21862fe82fdd1f64d9b
Alex Bodrov <alexvbodrov@mail.ru>
2023-07-24 23:38:18 +0300

    Форма выбора магазина, корректировки создания магазина

--- market_dashbord/models.py
+++ market_dashbord/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Market(models.Model):
     number_market = models.CharField(max_length=15,unique=True)
+    adress_market = models.CharField(max_length=65,unique=True)
 
     
     def __str__(self):
